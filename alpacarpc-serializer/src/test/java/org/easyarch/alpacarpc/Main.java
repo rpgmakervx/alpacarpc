011729c449e93f16045778c3b4ed2691efd54d28
rpgmakervx <583110127@qq.com>
2016-12-07 00:57:35 +0800

    初始化序列化对象

---
+++ alpacarpc-serializer/src/test/java/org/easyarch/alpacarpc/Main.java
@@ -0,0 +1,36 @@
+package org.easyarch.alpacarpc;
+
+import org.junit.Test;
+
+/**
+ * Description :
+ * Created by xingtianyu on 16-12-7
+ * 上午12:35
+ */
+
+public class Main {
+
+    @Test
+    public void test(){
+        System.out.println( " _| |______| |_\n" +
+                            "|              |\n" +
+                            "|      __      |\n" +
+                            "|  __|    |__  |\n" +
+                            "|   |      |   |\n" +
+                            "|              |\n" +
+                            "|      _|_     |\n" +
+                            "|              |\n" +
+                            "|__          __|\n" +
+                            "   |         |\n" +
+                            "   |         |\n" +
+                            "   |         |\n" +
+                            "   |         |______\n" +
+                            "   |                |_\n" +
+                            "   |  Alpaca RPC     _|\n" +
+                            "   |                |\n" +
+                            "   |___  ________  _|\n" +
+                            "      |  |      |  |\n" +
+                            "      |  |      |  |\n" +
+                            "      ||||      ||||\n");
+    }
+}
